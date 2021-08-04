ae7dfab577a6425ffe64be571ea80001c7d2f180
JayVince <13683085+JayVince@users.noreply.github.com>
2021-08-04 21:23:11 +0200

    Minor change in home page

--- examples/forum/templates/signin.php
+++ examples/forum/templates/signin.php
@@ -12,8 +12,6 @@
                          <div class="form-group">
                               <label for="loginEmail">Email address</label>
                               <input type="text" class="form-control" id="loginEmail" name="loginEmail" aria-describedby="emailHelp">
-                              <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                            else.</small> -->
                          </div>
                          <div class="form-group">
                               <label for="loginPass">Password</label>
